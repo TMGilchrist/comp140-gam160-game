b852987053de78c58fa056071de1d876910b8f30
TMGilchrist <telcontar@hotmail.co.uk>
2018-04-09 11:59:44 +0100

    Added TestSprite.png Cleaned up Main.cpp and added commented out code for blitting sprites.
    TODO:
    -Test sprite blitting.
    -Make player character.
    -Test player input.

--- ChairShip/ChairShip/Main.cpp
+++ ChairShip/ChairShip/Main.cpp
@@ -2,11 +2,14 @@
 //
 
 #include "stdafx.h"
-
 #include "SDL.h"
 
+//Screen dimensions
 const int SCREEN_HEIGHT = 600;
 const int SCREEN_WIDTH = 400;
+
+//Game loop runs while true
+bool gameRunning = true;
 
 int main(int argc, char *argv[])
 {
@@ -24,15 +27,26 @@ int main(int argc, char *argv[])
 	if (mainWindow == nullptr) 
 	{
 		std::cout << "Cannot create window " << SDL_GetError() << std::endl;
-		SDL_QUIT;
+		SDL_Quit();
 		return -1;
 	}
 
-	SDL_Event event;
-	int gameover = 0;
+	//load bitmap to temp surface
+	//SDL_Surface* temp = SDL_LoadBMP("sdl_logo.bmp");
 
-	//Main game loop
-	while (!gameover)
+	// convert bitmap to display format
+	//SDL_Surface* bg = SDL_DisplayFormat(temp);
+
+	// free the temp surface
+	//SDL_FreeSurface(temp);
+
+
+	SDL_Event event;
+
+	/*------------------------
+	      Main game loop 
+	------------------------*/
+	while (gameRunning)
 	{
 		//Check for SDL events
 		if (SDL_PollEvent(&event))
@@ -42,23 +56,35 @@ int main(int argc, char *argv[])
 			{
 				//Window closed
 				case SDL_QUIT:
-					gameover = 1;
+					gameRunning = false;
 					break;
 
-				//Escape key pressed
+				//Escape key pressed (could change to another switch statement for more keys)
 				case SDL_KEYDOWN:
 					if (event.key.keysym.sym == SDLK_ESCAPE)
 					{
-						gameover = 1;
+						gameRunning = false;
 						break;
 					}
 
 				break;
 			}
 		}
+
+		//draw the background
+		//SDL_BlitSurface(bg, NULL, screen, NULL);
+
+		//update the screen
+		//SDL_UpdateRect(screen, 0, 0, 0, 0);
+
 	}
 
+	//free the background surface */
+	//SDL_FreeSurface(bg);
 
-
+	//Close window
+	SDL_DestroyWindow(mainWindow);
+	SDL_Quit();
+	return 0;
 }
 
